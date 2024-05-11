a1ed070c6260d56e933a281f3ee00852b763d499
Abhirup-Choudhury <71977588+Abhirup-Choudhury@users.noreply.github.com>
2024-05-11 14:40:26 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,32 @@
 from flask import Flask, request, render_template  # type: ignore
-import pickle
+import tensorflow as tf
+from tensorflow.keras import models  # type: ignore
 import numpy as np
+import yfinance as yf
+
+# what to do
+# take the stock name as input
+# check if we have a model on the stock, if not, train
+# use the stockname to get the data point and predict.
+
 
 app = Flask(__name__)
 
-model = pickle.load(open('model.pkl', 'rb'))
+stockname = "AAPL"
+model = models.load_model(f'models/{stockname}_model')
+ticker = yf.Ticker(stockname)
+data = ticker.history()
 
 
 @app.route('/')
 def hello_world():
-    return render_template("fire.html")
+    return render_template("fire.html")  # change file name
 
 
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
     int_features = [int(x) for x in request.form.values()]
     final = [np.array(int_features)]
-    # print(int_features)
-    # print(final)
     prediction = model.predict_proba(final)
     output = '{0:.{1}f}'.format(prediction[0][1], 2)
 
